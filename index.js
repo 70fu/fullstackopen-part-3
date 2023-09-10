edf845e5cac053943556bd58a7734054a20e6b5e
Simon Reiser <ein.simonreiser@gmail.com>
2023-09-10 21:43:08 +0900

    complete exercises 3.15, 3.16

--- index.js
+++ index.js
@@ -9,44 +9,21 @@ const Person = require('./models/person')
 app.use(cors());
 app.use(express.json());
 app.use(express.static('frontend'))
-app.use(morgan((tokens, req,res)=>{
+app.use(morgan((tokens, req, res) => {
     let tokenArr = [
-        tokens.method(req,res),
-        tokens.url(req,res),
-        tokens.status(req,res),
-        tokens.res(req,res,'content-length'),'-',
-        tokens['response-time'](req,res), 'ms',
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
     ];
 
-    if(req.method === 'POST'){
+    if (req.method === 'POST') {
         tokenArr.push(JSON.stringify(req.body));
     }
 
     return tokenArr.join(' ');
 }));
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 
 /*app.get('/info', (request, response)=>{
     response.send(`
@@ -55,54 +32,70 @@ let persons = [
     `);
 })*/
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(people=>{
+app.get('/api/persons', (request, response, next) => {
+    Person.find({}).then(people => {
         response.json(people);
     })
+    .catch(error=>next(error));
 })
 
-app.get('/api/persons/:id', (request, response)=>{
-    Person.findById(request.params.id).then(person=>{
-        response.json(person);
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        //if person has been found, return
+        if (person) {
+            response.json(person);
+        }
+        else {
+            response.status(404).end();
+        }
     })
+    .catch(error=>next(error));
 })
 
-app.delete('/api/persons/:id', (request, response)=>{
-    return response.status(501).end();
-
-    /*const id = Number(request.params.id);
-
-    const person = persons.find((p)=>p.id===id);
-    if(person){
-        persons = persons.filter((p)=>p.id!==id);
-        response.status(204).end();
-    } else{
-        response.status(404).end();
-    }*/
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 })
 
-app.post('/api/persons', (request, response)=>{
+app.post('/api/persons', (request, response, next) => {
     const body = request.body;
 
     //check if body is valid
-    if(!body.name || !body.number){
-        return response.status(400).json({error:"name and number must be given"});
+    if (!body.name || !body.number) {
+        return response.status(400).json({ error: "name and number must be given" });
     }
     /*if(persons.find((p)=>p.name===body.name)){
         return response.status(400).json({error:"name must be unique"});
     }*/
 
     const person = new Person({
-        name:body.name,
-        number:body.number
+        name: body.name,
+        number: body.number
     });
 
-    person.save().then(savedPerson=>{
+    person.save().then(savedPerson => {
         response.status(201).send(savedPerson);
-    });
+    })
+    .catch(error=>next(error));
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
